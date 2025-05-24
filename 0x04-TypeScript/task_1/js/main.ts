971e82a06abe3a90b4c008091397d21953e38ff8
imanewkm <imanewkm02@gmail.com>
2025-05-24 15:10:10 +0100

    .\task_1\

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,24 @@ console.log(printTeacher("Imane", "Wakrim"));
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    constructor(private firstName: string, private lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
